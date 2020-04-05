78b67057cd4b5604f1faf4c1edb8e92a53034996
Dario Cazzani <dariocazzani@gmail.com>
2020-04-04 18:09:29 -0700

    Estimate variance

--- Chapter-5/normal-equation.py
+++ Chapter-5/normal-equation.py
@@ -9,17 +9,17 @@ args = parser.parse_args()
 
 """ Without regularization """
 # 1 Dimensional
-num_samples = 200
+num_samples = 100
 num_features = 1
 theta = np.ones((1,1))* 3.14
-bias = np.ones((1,1)) * 4.2
-noise_sigma = 2
+offset = np.ones((1,1)) * 4.2
+noise_sigma = 3
 X_train = np.arange(-num_samples, num_samples+1)
 num_samples = len(X_train)
 X_train = X_train[:, None]
 # X_train = np.random.randn(num_samples, num_features)
-X_train_bias = np.hstack((X_train, np.ones((num_samples, 1))))
-y_train = np.matmul(X_train, theta) + bias
+X_train_offset = np.hstack((X_train, np.ones((num_samples, 1))))
+y_train = np.matmul(X_train, theta) + offset
 y_train_noise = y_train + noise_sigma * np.random.randn(*y_train.shape)
 
 if args.plot:
@@ -28,32 +28,32 @@ if args.plot:
     plt.grid()
     plt.show()
 
-w = np.matmul(np.matmul(inv(np.matmul(X_train_bias.T, X_train_bias)), X_train_bias.T), y_train_noise)
+w = np.matmul(np.matmul(inv(np.matmul(X_train_offset.T, X_train_offset)), X_train_offset.T), y_train_noise)
 theta_hat = w[:-1]
-bias_hat = w[-1]
+offset_hat = w[-1]
 print(f"{num_features} Dimensions")
 print(f"Theta:\n{theta} \nTheta_hat:\n{theta_hat}")
-print(f"\nBias:\n{bias} \nBias_hat:\n{bias_hat}")
+print(f"\nOffset:\n{offset} \nOffset_hat:\n{offset_hat}")
 
-# Compute bias of sample mean (see page 123) - Distribution is not Gaussian but the samples are simmetric around 0
-gaussian_mean = np.mean(y_train_noise) - np.mean(y_train)
-print(f"\nBias of sample mean: {gaussian_mean}")
-# which is the same as bias_hat - bias:
-print(f"bias_hat - bias:     {float(bias_hat-bias)}")
+# Compute an unbiased estimate of the variance (of noise) as the MSE
+y_pred = np.matmul(X_train, theta_hat) + offset_hat
+noise_variance_hat = np.sum((y_pred-y_train)**2) / (num_samples-2)
+print(f"\nOriginal variance: {noise_sigma**2}")
+print(f"Estimated variance: {noise_variance_hat}")
 
 print("\n*************")
 # n Dimensional
 num_samples = 10
 num_features = 3
 theta = np.random.randn(3, 1)
-bias = np.random.randn(1,1)
+offset = np.random.randn(1,1)
 X_train = np.random.randn(num_samples, num_features)
-X_train_bias = np.hstack((X_train, np.ones((num_samples, 1))))
-y_train = np.matmul(X_train, theta) + bias
+X_train_offset = np.hstack((X_train, np.ones((num_samples, 1))))
+y_train = np.matmul(X_train, theta) + offset
 
-w = np.matmul(np.matmul(inv(np.matmul(X_train_bias.T, X_train_bias)), X_train_bias.T), y_train)
+w = np.matmul(np.matmul(inv(np.matmul(X_train_offset.T, X_train_offset)), X_train_offset.T), y_train)
 theta_hat = w[:-1]
-bias_hat = w[-1]
+offset_hat = w[-1]
 print(f"\n{num_features} Dimensions")
 print(f"Theta:\n{theta} \nTheta_hat:\n{theta_hat}")
-print(f"\nBias:\n{bias} \nBias_hat:\n{bias_hat}")
+print(f"\nOffset:\n{offset} \nOffset_hat:\n{offset_hat}")
